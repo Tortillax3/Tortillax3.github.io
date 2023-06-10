4650260dc4f8c61cd2a9382279b1a13c070820ed
Tortillax3 <roman53680@gmail.com>
2023-06-10 04:23:45 +0100

    Updated style of site, created navbar, logo, favicon and mmany other

--- RStudio/RStudio/settings.py
+++ RStudio/RStudio/settings.py
@@ -125,3 +125,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
