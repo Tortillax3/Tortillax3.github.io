e9659faf5d86dbc4d4e0da176569116dab76f82f
Tortillax3 <roman53680@gmail.com>
2023-06-10 02:21:07 +0100

    connected css, boostrap

--- RStudio/RStudio/settings.py
+++ RStudio/RStudio/settings.py
@@ -16,6 +16,7 @@ import os, sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Studio',
 ]
 
 MIDDLEWARE = [
